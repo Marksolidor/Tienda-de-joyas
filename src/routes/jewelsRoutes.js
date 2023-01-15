c1dea74c60f5961eabf887edc794e4b5d070e972
Marksolidor <marco350sl@gmail.com>
2023-01-15 12:50:33 -0300

    feat is added the code for the connection to the DB and the path file of the MVC pattern

--- src/routes/jewelsRoutes.js
+++ src/routes/jewelsRoutes.js
@@ -1,1 +1,31 @@
-//jewelsRoures goes here
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllJewels,
+  getAllJewelsForFilters,
+} = require("../controllers/jewelsController");
+
+router.get("/inventario", (req, res) => {
+  try {
+    getAllJewels(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("/inventario/filtros", (req, res) => {
+  try {
+    getAllJewelsForFilters(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("*", (req, res) => {
+  res.status(404).send("Esta ruta no existe");
+});
+
+module.exports = router;
